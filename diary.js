3ba0e56e3f981d39e76c2be2bd6cbf1d8a530c91
Lakshmi <lakshmi.nishthala@gmail.com>
2017-11-27 15:58:48 -0800

    Trying test case 3

--- diary.js
+++ diary.js
@@ -1,20 +1,30 @@
 class Diary {
-constructor(){
-	this.diary = {};
-}
+	constructor() {
+		this.diary = {};
+	}
 
-entry(str, date){
-let len = Object.keys(this.diary).length || 0
-let currentTime = new Date();
-if (date) {
-this.diary[date] = str;
-} else {
-this.diary[currentTime] = str;
-}
-return [str,date];
-}
+	entry(str, date) {
+		let len = Object.keys(this.diary).length || 0;
+		let currentTime = new Date().toISOString().slice(0, 19);
+		if (date) {
+			this.diary[date] = str;
+			return [str, date];
+		} else {
+			date = currentTime;
+			this.diary[date] = str;
+			return str;
+		}
+	}
 
-}
+	entries() {
+		let arr = [];
+		for (let i in this.diary) {
+			arr.push(this.diary[i]);
+		}
+		return Object.keys(this.diary);
+	}
 
+	//entry(str, data = Date.now) {}
+}
 
 module.exports = Diary;
--- diary.spec.js
+++ diary.spec.js
@@ -2,25 +2,32 @@ let Diary = require('./diary');
 
 let diary = new Diary();
 
-describe("entry()", () => {
-it('adds entry to diary', () => {
-expect(diary.entry("Brad is everything to me.")).toEqual('Brad is everything to me.');
+describe('entry()', () => {
+	it('adds entry to diary', () => {
+		expect(diary.entry('Brad is everything to me.')).toEqual(
+			'Brad is everything to me.'
+		);
+	});
+
+	it('contains time/date of its creation', () => {
+		expect(diary.entry('<3 Braaaad <3')).toEqual('<3 Braaaad <3');
+	});
+
+	// it('may take an optional date argument which will be set as its time of creation', () => {
+	// 	expect(diary.entry('dkeidk', 'Mon, 25 Dec 1995 13:30:00 GMT')).toEqual([
+	// 		'dkeidk',
+	// 		819898200000
+	// 	]);
+	// });
 });
 
-it('contains time/date of its creation', () => {
-expect(diary.entry("<3 Braaaad <3")).toEqual('<3 Braaaad <3' + diary[this.currentTime]);
+describe('entries()', () => {
+	let obj;
+	// beforeEach(function() {
+	// 	obj = new Diary();
+	// });
+	it('returns list of all entries', () => {
+		expect(diary.entries()).toEqual(Object.keys(this.diary));
+	});
 });
-
-
-it('may take an optional date argument which will be set as its time of creation', () =>{
-expect(diary.entry("dkeidk", Date.parse("Mon, 25 Dec 1995 13:30:00 GMT"))).toEqual(['dkeidk', 819898200000]);
-
-
-})
-
-});
-
-
-
-
-console.log(typeof(diary))
+console.log(typeof diary);
