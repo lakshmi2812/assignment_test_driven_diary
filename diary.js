9207f8ca3930b93e4c2f70ba79faa603a35771bc
Lakshmi <lakshmi.nishthala@gmail.com>
2017-11-27 19:00:02 -0800

    First three tests passed

--- diary.js
+++ diary.js
@@ -1,30 +1,37 @@
 class Diary {
 	constructor() {
-		this.diary = {};
+		this._entries = [];
 	}
-
-	entry(str, date) {
-		let len = Object.keys(this.diary).length || 0;
-		let currentTime = new Date().toISOString().slice(0, 19);
-		if (date) {
-			this.diary[date] = str;
-			return [str, date];
-		} else {
-			date = currentTime;
-			this.diary[date] = str;
-			return str;
-		}
+	entry(str, date = new Date()) {
+		date = date.toISOString().substr(0, 19);
+		this._entries.push({ date: date, msg: str });
 	}
 
 	entries() {
-		let arr = [];
-		for (let i in this.diary) {
-			arr.push(this.diary[i]);
-		}
-		return Object.keys(this.diary);
+		//return array of entry objects
+		return this._entries;
 	}
-
-	//entry(str, data = Date.now) {}
 }
 
 module.exports = Diary;
+
+// entry(str, date) {
+// 	let len = Object.keys(this.diary).length || 0;
+// 	let currentTime = new Date().toISOString().slice(0, 19);
+// 	if (date) {
+// 		this.diary[date] = str;
+// 		return [str, date];
+// 	} else {
+// 		date = currentTime;
+// 		this.diary[date] = str;
+// 		return str;
+// 	}
+// }
+//
+// entries() {
+// 	let arr = [];
+// 	for (let i in Object.keys(this.diary)) {
+// 		arr.push(this.diary[i]);
+// 	}
+// 	return arr;
+// }
--- diary.spec.js
+++ diary.spec.js
@@ -2,32 +2,44 @@ let Diary = require('./diary');
 
 let diary = new Diary();
 
-describe('entry()', () => {
-	it('adds entry to diary', () => {
-		expect(diary.entry('Brad is everything to me.')).toEqual(
-			'Brad is everything to me.'
-		);
-	});
+describe('Diary', () => {
+	describe('entry()', () => {
+		let obj;
+		beforeEach(function() {
+			obj = new Diary();
+		});
 
-	it('contains time/date of its creation', () => {
-		expect(diary.entry('<3 Braaaad <3')).toEqual('<3 Braaaad <3');
-	});
+		it('adds entry to diary', () => {
+			expect(obj.entries().length).toEqual(0);
+			obj.entry('Brad is everything to me.');
+			expect(obj.entries().length).toEqual(1);
+		});
 
-	// it('may take an optional date argument which will be set as its time of creation', () => {
-	// 	expect(diary.entry('dkeidk', 'Mon, 25 Dec 1995 13:30:00 GMT')).toEqual([
-	// 		'dkeidk',
-	// 		819898200000
-	// 	]);
-	// });
+		it('contains time/date of its creation', () => {
+			obj.entry('Hello World!');
+			const then = obj.entries()[0].date;
+			const now = new Date().toISOString().slice(0, 19);
+			expect(then).toEqual(now);
+		});
+
+		it('may take an optional date argument which will be set as its time of creation', () => {
+			obj.entry('dkeidk', new Date('Mon, 25 Dec 1995 13:30:00 GMT'));
+			let testDate = new Date('Mon, 25 Dec 1995 13:30:00 GMT')
+				.toISOString()
+				.substr(0, 19);
+			expect(obj.entries()[0].date).toEqual(testDate);
+		});
+	});
 });
 
-describe('entries()', () => {
-	let obj;
-	// beforeEach(function() {
-	// 	obj = new Diary();
-	// });
-	it('returns list of all entries', () => {
-		expect(diary.entries()).toEqual(Object.keys(this.diary));
-	});
-});
-console.log(typeof diary);
+//
+// describe('entries()', () => {
+// 	let obj;
+// 	// beforeEach(function() {
+// 	// 	obj = new Diary();
+// 	// });
+// 	it('returns list of all entries', () => {
+// 		expect(diary.entries()).toEqual(Object.values(diary.diary));
+// 	});
+// });
+// console.log(typeof diary);
